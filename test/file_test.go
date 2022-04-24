6102e700776c7dc0c57c36a05b81228d8fbadac9
zhaopan <2272795814@qq.com>
2022-04-24 23:00:17 +0800

    test : 分片的生成、合并、一致性校验

---
+++ test/file_test.go
@@ -0,0 +1,101 @@
+package test
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"testing"
+)
+
+// 分片的大小
+const chunkSize = 100 * 1024 * 1024 // 100MB
+
+// 文件分片
+func TestGenerateChunkFile(t *testing.T) {
+	fileInfo, err := os.Stat("test.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// 分片的个数
+	chunkNum := math.Ceil(float64(fileInfo.Size()) / float64(chunkSize))
+	myFile, err := os.OpenFile("test.mp4", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := make([]byte, chunkSize)
+	for i := 0; i < int(chunkNum); i++ {
+		// 指定读取文件的起始位置
+		myFile.Seek(int64(i*chunkSize), 0)
+		if chunkSize > fileInfo.Size()-int64(i*chunkSize) {
+			b = make([]byte, fileInfo.Size()-int64(i*chunkSize))
+		}
+		myFile.Read(b)
+
+		f, err := os.OpenFile("./"+strconv.Itoa(i)+".chunk", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+		f.Write(b)
+		f.Close()
+	}
+	myFile.Close()
+}
+
+// 分片文件的合并
+func TestMergeChunkFile(t *testing.T) {
+	myFile, err := os.OpenFile("test2.mp4", os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fileInfo, err := os.Stat("test.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// 分片的个数
+	chunkNum := math.Ceil(float64(fileInfo.Size()) / float64(chunkSize))
+	for i := 0; i < int(chunkNum); i++ {
+		f, err := os.OpenFile("./"+strconv.Itoa(i)+".chunk", os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		myFile.Write(b)
+		f.Close()
+	}
+	myFile.Close()
+}
+
+// 文件一致性校验
+func TestCheck(t *testing.T) {
+	// 获取第一个文件的信息
+	file1, err := os.OpenFile("test.mp4", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b1, err := ioutil.ReadAll(file1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// 获取第二个文件的信息
+	file2, err := os.OpenFile("test2.mp4", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b2, err := ioutil.ReadAll(file2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	s1 := fmt.Sprintf("%x", md5.Sum(b1))
+	s2 := fmt.Sprintf("%x", md5.Sum(b2))
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s1 == s2)
+}
